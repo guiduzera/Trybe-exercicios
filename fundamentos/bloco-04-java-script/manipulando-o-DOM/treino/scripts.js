2ec510fa9c32fcead7a6aa3fc168852d4a0e6d59
Guilherme Eduardo <guigui.carvalho15@gmail.com>
2022-02-06 01:27:29 -0300

    exercicio para treinar modificacao de estilos com JS

---
+++ fundamentos/bloco-04-java-script/manipulando-o-DOM/treino/scripts.js
@@ -0,0 +1,24 @@
+const header = document.querySelector("#header-container");
+header.style.backgroundColor = "green";
+
+const bubonica = document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+
+document.querySelectorAll(".h4")[0].style.backgroundColor = "black";
+
+document.querySelectorAll(".h4")[1].style.backgroundColor = "black";
+
+document.querySelector("#footer-container").style.backgroundColor = "green";
+
+const section = document.querySelector(".emergency-tasks");
+section.style.backgroundColor = "pink";
+
+const contentSection = document.getElementsByClassName("h3")
+for (let i = 0; i <= contentSection.length; i++) {
+    contentSection[i].style.backgroundColor = "purple";
+}
+
+
+
+
+
+
