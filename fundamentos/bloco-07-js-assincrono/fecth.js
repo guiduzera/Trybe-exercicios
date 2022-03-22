62cd294598220a470c786e408da8d96c7a697375
Guilherme Eduardo <guigui.carvalho15@gmail.com>
2022-03-22 16:30:18 -0300

    y

--- fundamentos/bloco-07-js-assincrono/fecth.js
+++ fundamentos/bloco-07-js-assincrono/fecth.js
@@ -1,15 +1,31 @@
 const fetch = require('node-fetch');
 
-const fetchJoke = async () => {
-  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+const fetchJoke = async (url) => {
+
+  const u = url;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(u);
     const data = await response.json();
     console.log(data.value);
-  } catch(error) {
-    console.log(`Algo deu errado :( \n${error}`);
+    if (data.value === undefined) {
+      throw new Error('vish, deu ruim!')
+    }
+  } catch (error) {
+    console.log(`Algo deu errado :( \n${error.message}`);
   }
 }
 
-fetchJoke();
+fetchJoke('https://api.chucknorris.io/jokes/random?category=dev');
+
+//fetch(url)
+// .then((response) => { 
+// if(!response.ok) throw new Error('vish, deu ruim!');
+// return response.json();
+// })
+// .then((data) => { 
+// return console.log(data.value);
+// })
+// .catch((err) => {
+// return console.log(err.message);
+// })
